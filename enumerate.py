f3caee303e153873701fbd40b32dd0a29525ce8a
elyasn <elyasn@cloudbloq.io>
2023-01-11 07:29:50 +0430

    enumerate and continute of quiz project

---
+++ enumerate.py
@@ -0,0 +1,5 @@
+mylist = ["mustafa", "khan", "maryam"]
+letter = ['a','b','c']
+for label, names in enumerate(mylist, start=1):        
+    print(f"{label}) {names}")
+    
--- project1.py
+++ project1.py
@@ -1,24 +1,50 @@
+from string import ascii_lowercase
+import emoji
 # questions = [
 #     ("when was invented internet", "1970"),
 #     ("who invented python, ", "vanrossum")
 # ]
+# enumerate()  -> count, value
+
 questions = {
     "which one is a loop in python?" : ["for", "none", "if", "switch"],
     "which one is a correct variale in python" : ["name", "name123", "if", "break"],
     "with which language we build dynamic websites?" : ["python", "css", "html", "none"],
     "which one is a keyword in python" : ["def", "iff", "elsee", "function"],
 }
-
-for question, alternatives in questions.items():
+# letters = ['','a','b','c','d']
+for num , (question, alternatives) in enumerate(questions.items(), start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}")
     correct_answer = alternatives[0]
-    for alternative in sorted(alternatives):
-        print(f" {alternative}")
+    labeled_choices = dict(zip(ascii_lowercase, sorted(alternatives)))
 
-    answer = input(f"{question}? ")
+    for label, choice in labeled_choices.items():
+        print(f" {label}) {choice}")
+
+    answer_label = input(f"\nChoice? ")
+    answer = labeled_choices.get(answer_label)
     if answer == correct_answer:
-        print(" correct!")
+        print(" \n{winking face}  correct!")
     else: 
         print(f"The answer is {correct_answer}, not {answer}")
+
+
+
+
+
+# for question, alternatives in questions.items():
+#     correct_answer = alternatives[0]
+#     sorted_choices = sorted(alternatives)
+#     for label, choice in enumerate(sorted_choices):
+#         print(f" {label}) {choice}")
+
+#     answer_label = int(input(f"{question} "))
+#     answer = sorted_choices[answer_label]
+#     if answer == correct_answer:
+#         print(" correct!")
+#     else: 
+#         print(f"The answer is {correct_answer}, not {answer}")
         
         
     
